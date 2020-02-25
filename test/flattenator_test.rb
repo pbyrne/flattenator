184f48c782300a3447e23e1e7e0e09cef494d318
Patrick Byrne <34053+pbyrne@users.noreply.github.com>
2020-02-25 13:27:42 -0600

    Get test suite passing
    Because:
    
    * It's only as valuable if it's green
    
    This commit:
    
    * Switches to spec-style tests
    * Removes the intentionally failing assertion

--- test/flattenator_test.rb
+++ test/flattenator_test.rb
@@ -1,11 +1,9 @@
 require "test_helper"
 
-class FlattenatorTest < Minitest::Test
-  def test_that_it_has_a_version_number
-    refute_nil ::Flattenator::VERSION
-  end
-
-  def test_it_does_something_useful
-    assert false
+describe Flattenator do
+  describe "::VERSION" do
+    it "exists" do
+      refute_nil Flattenator::VERSION
+    end
   end
 end
