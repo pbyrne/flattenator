7e26887e55d439bdd6269b5b9516412c9f83ba39
Patrick Byrne <34053+pbyrne@users.noreply.github.com>
2020-02-25 11:58:22 -0600

    bundle gem flattenator

---
+++ lib/flattenator.rb
@@ -0,0 +1,6 @@
+require "flattenator/version"
+
+module Flattenator
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/flattenator/version.rb
@@ -0,0 +1,3 @@
+module Flattenator
+  VERSION = "0.1.0"
+end
